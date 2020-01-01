f912a4da1188b94300a713f679153c490d457274
박영호 <mellonia.bluerose@gmail.com>
2020-01-01 20:43:00 +0900

    apply redux, router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./modules";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import ReduxThunk from "redux-thunk";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const customHistory = createBrowserHistory();
+const store = createStore(
+  rootReducer,
+  applyMiddleware(ReduxThunk.withExtraArgument({ history: customHistory }))
+);
+
+ReactDOM.render(
+  <Router history={customHistory}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
