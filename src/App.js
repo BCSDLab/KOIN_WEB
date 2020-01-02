e09bd43e25c0cc1e7f13c589746cea902afbaa38
박영호 <mellonia.bluerose@gmail.com>
2020-01-02 14:19:39 +0900

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+    text-align: center;
+  }
+
+  * {
+    outline: none;
+  }
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Switch>
+        <Route exact path="/" component={IndexPage} />
+        <Route path="/login" component={LoginPage} />
+      </Switch>
+    </>
   );
 }
 
