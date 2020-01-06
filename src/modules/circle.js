db4a6e59f27025b357d226f2a556726003209a46
박영호 <mellonia.bluerose@gmail.com>
2020-01-07 02:48:29 +0900

    feat: Add get circle info action creator
    Add get circle info action creator. Update initial state of
    circle reducer.

--- src/modules/circle.js
+++ src/modules/circle.js
@@ -6,6 +6,10 @@ const GET_CIRCLE_LIST_ERROR = "GET_CIRCLE_ERROR";
 
 const GET_CIRCLE_LIST_BY_CATEGORY = "GET_CIRCLE_LIST_BY_CATEGORY";
 
+const GET_CIRCLE_DETAIL_INFO = "GET_CIRCLE_DETAIL_INFO";
+const GET_CIRCLE_DETAIL_INFO_SUCCESS = "GET_CIRCLE_DETAIL_INFO_SUCCESS";
+const GET_CIRCLE_DETAIL_INFO_ERROR = "GET_CIRCLE_DETAIL_INFO_ERROR";
+
 export const getCircleList = tag => async dispatch => {
   dispatch({ type: GET_CIRCLE_LIST });
   try {
@@ -31,12 +35,34 @@ export const getCircleListByCategory = tag => ({
   tag
 });
 
+export const getCircleDetailInfo = id => async dispatch => {
+  dispatch({ type: GET_CIRCLE_DETAIL_INFO });
+  try {
+    const res = await infoAPI.getCircleInfo(id);
+    dispatch({
+      type: GET_CIRCLE_DETAIL_INFO_SUCCESS,
+      res
+    })
+  } catch(e) {
+    dispatch({
+      type: GET_CIRCLE_DETAIL_INFO_ERROR,
+      error: e
+    })
+  }
+};
+
 const initialState = {
-  loading: false,
-  circleList: [],
-  filteredCircleList: [],
-  selectedCircleInfo: null,
-  error: null
+  circles: {
+    loading: false,
+    data: [],
+    filteredData: [],
+    error: null
+  },
+  circle: {
+    loading: false,
+    data: null,
+    error: null
+  }
 };
 
 export default function circleReducer(state = initialState, action) {
@@ -44,29 +70,73 @@ export default function circleReducer(state = initialState, action) {
     case GET_CIRCLE_LIST:
       return {
         ...state,
-        loading: true
+        circles: {
+          loading: true,
+          data: [],
+          filteredData: [],
+          error: null
+        }
       };
     case GET_CIRCLE_LIST_SUCCESS:
       return {
         ...state,
-        loading: false,
-        circleList: action.res.data.circles,
-        filteredCircleList: action.res.data.circles
+        circles: {
+          loading: false,
+          data: action.res.data.circles,
+          filteredData: action.res.data.circles,
+          error: null
+        }
       };
     case GET_CIRCLE_LIST_ERROR:
       return {
         ...state,
-        loading: false,
-        error: action.error
+        circles: {
+          loading: false,
+          data: null,
+          filteredData: null,
+          error: action.error
+        }
       };
     case GET_CIRCLE_LIST_BY_CATEGORY:
       return {
         ...state,
-        filteredCircleList:
-          action.tag === "ALL"
-            ? state.circleList
-            : state.circleList.filter(circle => circle.category === action.tag)
+        circles: {
+          loading: false,
+          data: state.circles.data,
+          filteredData:
+            action.tag === "ALL"
+              ? state.circles.data
+              : state.circles.data.filter(circle => circle.category === action.tag),
+          error: null
+        }
       };
+    case GET_CIRCLE_DETAIL_INFO:
+      return {
+        ...state,
+        circle: {
+          loading: true,
+          data: null,
+          error: null,
+        }
+      }
+    case GET_CIRCLE_DETAIL_INFO_SUCCESS:
+      return {
+        ...state,
+        circle: {
+          loading: false,
+          data: action.res.data,
+          error: null
+        }
+      }
+    case GET_CIRCLE_DETAIL_INFO_ERROR:
+      return {
+        ...state,
+        circle: {
+          loading: false,
+          data: null,
+          error: action.error
+        }
+      }
     default:
       return state;
   }
