7218de0b483111873fcf4dd53c0d24800039676c
박영호 <mellonia.bluerose@gmail.com>
2020-01-10 01:59:48 +0900

    refactor: Remove needless mouse event logic in room list container
    Remove needless mouse event logic in room list container.

--- src/containers/RoomListContainer.js
+++ src/containers/RoomListContainer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import RoomList from '../components/RoomList';
 import { useSelector, useDispatch } from 'react-redux';
 import { getRoomList } from '../modules/room';
@@ -6,25 +6,6 @@ import { getRoomList } from '../modules/room';
 export default function RoomListContainer({ history }) {
   const dispatch = useDispatch();
   const { data, loading, error } = useSelector(state => state.roomReducer.rooms);
-
-  const [marker, setMarker] = useState({
-    id: null,
-    isActive: false
-  });
-
-  const onMouseOverMarker = id => {
-    setMarker({
-      id: id,
-      isActive: true
-    })
-  }
-
-  const onMouseOutMarker = () => {
-    setMarker({
-      id: null,
-      isActive: false
-    })
-  }
 
   useEffect(() => {
     dispatch(getRoomList());
@@ -34,9 +15,6 @@ export default function RoomListContainer({ history }) {
     <RoomList
       roomList={data}
       loading={loading}
-      marker={marker}
-      onMouseOverMarker={onMouseOverMarker}
-      onMouseOutMarker={onMouseOutMarker}
       history={history}
     />
   )
