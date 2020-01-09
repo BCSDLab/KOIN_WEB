789a39f77339cc2402d3ff1a249d0b09fe66e31b
박영호 <mellonia.bluerose@gmail.com>
2020-01-09 16:32:43 +0900

    feat: Add room reducer
    Add room room reducer on modules.

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import authReducer from './auth';
 import circleReducer from './circle';
+import roomReducer from './room';
 
 const rootReducer = combineReducers({
   authReducer,
-  circleReducer
+  circleReducer,
+  roomReducer
 });
 
 export default rootReducer;
