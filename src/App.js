fe9eb6ef6501d813a3d980a89a668f092421220c
박영호 <mellonia.bluerose@gmail.com>
2020-01-21 21:30:55 +0900

    feat: Apply private route
    Apply private route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
 import IndexPage from './pages/IndexPage';
-import LoginPage from './pages/LoginPage';
+
+import LoginPage from './pages/UserPages/LoginPage';
+import SignUpPage from './pages/UserPages/SignUpPage';
+import ModifyInfoPage from './pages/UserPages/ModifyInfoPage';
+import FindPasswordPage from './pages/UserPages/FindPasswordPage';
+
 import FaqPage from "./pages/FaqPage";
-import SignUpPage from './pages/SignUpPage';
-import FindPasswordPage from './pages/FindPasswordPage';
 import CircleListPage from './pages/CircleListPage';
 import CircleDetailPage from './pages/CircleDetailPage';
 
@@ -12,7 +16,13 @@ import RoomListPage from './pages/RoomListPage';
 import RoomDetailPage from './pages/RoomDetailPage';
 import Footer from './components/SharedComponents/Footer/Footer'
 import page404 from './pages/404';
+import TopnavContainer from './containers/TopnavContainer';
+
 import styled, { createGlobalStyle } from 'styled-components';
+import { updateAuthInfo } from './modules/auth';
+import { useDispatch } from 'react-redux';
+import PrivateRoute from './components/PrivateRoute';
+import Cookies from 'js-cookie';
 
 const GlobalStyle = createGlobalStyle`
   @font-face {
@@ -69,18 +79,69 @@ const Main = styled.main`
     height: calc(100% - 130px);
     min-height: calc(100% - 130px);
   }
-`
+`;
 
-function App() {
+
+function App({ history }) {
+  const dispatch = useDispatch();
+  const [dialog, setDialog] = useState(false);
+  
+  const onConfirm = path => {
+    setDialog(false);
+    if (path) history.push(path);
+    else history.goBack();
+  }
+
+  useEffect(() => {
+    // 자동 로그인이 켜져있을 경우.
+    if (Cookies.get("token")) {
+      sessionStorage.setItem("token", Cookies.get("token"));
+    }
+    // 글로벌 토큰, 유저정보 유지
+    dispatch(updateAuthInfo(
+      sessionStorage.getItem("token"), JSON.parse(sessionStorage.getItem("userInfo"))
+    ));
+    // sessionStorage.setItem("storeCheckbboxs", [false, false, false]);
+    // sessionStorage.setItem("storeTag", null);
+    // sessionStorage.setItem("storeNewFlag", true);
+
+  }, []);
+
   return (
     <>
       <GlobalStyle />
+      <TopnavContainer />
       <Main>
         <Switch>
           <Route exact path="/" component={IndexPage} />
-          <Route path="/login" component={LoginPage} />
-          <Route path="/signup" component={SignUpPage} />
-          <Route path="/findpw" component={FindPasswordPage} />
+          <PrivateRoute 
+            path="/login"
+            component={LoginPage}
+            setDialog={setDialog}
+            dialog={dialog}
+            onConfirm={onConfirm}
+          />
+          <PrivateRoute 
+            path="/signup"
+            component={SignUpPage}
+            setDialog={setDialog}
+            dialog={dialog}
+            onConfirm={onConfirm}
+          />
+          <PrivateRoute 
+            path="/modifyinfo"
+            component={ModifyInfoPage}
+            setDialog={setDialog}
+            dialog={dialog}
+            onConfirm={onConfirm}
+          />
+          <PrivateRoute
+            path="/findpw"
+            component={FindPasswordPage}
+            setDialog={setDialog}
+            dialog={dialog}
+            onConfirm={onConfirm}
+          />
 
           <Route exact path="/circle" component={CircleListPage} />
           <Route path="/circle/:id" component={CircleDetailPage} />
