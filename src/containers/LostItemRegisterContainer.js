458377ad71cc4d6c1203ddc0129fa45e897da520
ChoiWonBeen <zad1264@naver.com>
2020-02-08 01:59:42 +0900

    feat: Add toast ui editor

--- src/containers/LostItemRegisterContainer.js
+++ src/containers/LostItemRegisterContainer.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import LostItemRegister from "../components/LostItemRegister";
 
 export default function LostItemRegisterContainer() {
+  const offset = new Date().getTimezoneOffset() * 60000;
+  const today = new Date(Date.now() - offset);
+  const [type, setType] = useState(0);
+  const [phoneFlag, setPhoneFlag] = useState(0);
+
+  const createdAt = today.toISOString().slice(0,10).replace('-','. ').replace('-','. ');
+
+  const clickType = (type) => () => {
+    setType(type)
+  };
+
+  const phoneFlagChange = (flag) => () => {
+    setPhoneFlag(flag)
+  };
+
+
+
   return (
-    <LostItemRegister/>
+    <LostItemRegister
+      createdAt={createdAt}
+      type={type}
+      clickType={clickType}
+      phoneFlag={phoneFlag}
+      phoneFlagChange={phoneFlagChange}/>
   )
 }
