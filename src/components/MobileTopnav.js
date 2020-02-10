351a73d17625d36108d6d6fc19906876e83bd28e
박영호 <mellonia.bluerose@gmail.com>
2020-02-11 02:00:40 +0900

    feat: Add mobile top nav
    Add mobile top navigation. It 	is consist of
    slide enter animation. Just click hamburger button,
    User can see easily menu.

---
+++ src/components/MobileTopnav.js
@@ -0,0 +1,147 @@
+import React from 'react'
+import styled, { css } from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const Container = styled.div`
+  position: fixed;
+  top: 55px;
+  left: 0;
+  width: 100%;
+  height: calc(100% - 153px);
+  min-width: 360px;
+  background: #f7f7f7;
+  padding-bottom: 40px;
+  
+  border-bottom: rgba(210, 218, 226, 0.5) 1px solid;
+  transition: all .3s cubic-bezier(0, 0, 0.2, 1);
+  transform: ${props => props.mobileMenu ? 'none' : 'translateX(-100%)'};
+  z-index: 20;
+`;
+
+const UserInfoSection = styled.div`
+  width: 100%;
+  height: 41px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #175c8e;
+  font-size: 14px;
+  font-family: NanumSquare;
+  padding: 0 20px;
+  box-sizing: border-box;
+  background: #ffffff;
+`;
+
+const AuthSection = styled.div`
+  width: 100%;
+`;
+
+
+const Nickname = styled.span`
+  font-size: 16px;
+`;
+
+const AuthLinkStyle = css`
+  cursor: pointer;
+  margin-right: 9px;
+  height: 12px;
+  text-decoration: none;
+  color: #175c8e;
+  & + & {
+    margin-left: 9px;
+    margin-right: 0;
+  }
+`;
+
+const StyledAuthLink = styled(Link)`
+  ${AuthLinkStyle}
+`;
+const AuthLinkButton = styled.span`
+  cursor: pointer;
+  margin-left: 9px;
+`
+
+const TitleSection = styled.div`
+  width: 100%;
+  box-sizing: border-box;
+  padding: 24px 20px 10px 20px;
+  font-size: 14px;
+  color: #858585;
+  text-align: left;
+`;
+
+const SubMenuWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+`
+const SubMenuLinkButton = styled(Link)`
+  display: block;
+  width: 50%;
+  background: #fff;
+  border: 0.5px rgba(210, 218, 226, 0.5) solid;
+  font-size: 16px;
+  box-sizing: border-box;
+  border-collapse: collapse;
+  cursor: pointer;
+  text-decoration: none;
+  padding: 11px 20px;
+  text-align: left;
+  color: #252525;
+  font-weight: 500;
+
+  &:hover {
+    color: #f7941e;
+    font-weight: 700;
+    outline: none;
+  }
+`;
+
+
+export default function MobileTopnav({
+  mobileMenu,
+  token,
+  userInfo,
+  categories,
+  onLogout,
+  setMobileMenu
+}) {
+  return (
+    <Container mobileMenu={mobileMenu}>
+      <UserInfoSection>
+        <Nickname>
+          {userInfo && userInfo.nickname}
+        </Nickname>
+        <div>
+          {token && 
+            <>
+              <StyledAuthLink to="/modifyinfo" onClick={() => setMobileMenu(false)}>정보수정</StyledAuthLink>|
+              <AuthLinkButton onClick={() => {onLogout(); setMobileMenu(false)}}>로그아웃</AuthLinkButton>
+            </>
+          }
+          {!token &&
+            <>
+              <StyledAuthLink to="/signup" onClick={() => setMobileMenu(false)}>회원가입</StyledAuthLink>|
+              <StyledAuthLink to="/login" onClick={() => setMobileMenu(false)}>로그인</StyledAuthLink>
+            </>
+          }
+        </div>
+      </UserInfoSection>
+      {categories.map((category, idx) => (
+        <div key={idx}>
+          <TitleSection>
+            {category.title}
+          </TitleSection>
+          <SubMenuWrapper>
+          {category.submenu.map((submenu, idx2) => (
+            <SubMenuLinkButton key={idx2} to={submenu.link} onClick={() => setMobileMenu(false)}>
+              {submenu.title}
+            </SubMenuLinkButton>
+          ))}
+          </SubMenuWrapper>
+          
+        </div>
+      ))}
+    </Container>
+  )
+}
