5226ccf7ae520ee5358d9c781f57735503346045
Yunsup Sim <pedogunu@gmail.com>
2020-05-04 04:59:06 +0900

    feat: Update testutills

--- src/lib/testUtils.js
+++ src/lib/testUtils.js
@@ -1,27 +1,29 @@
 import React from 'react'
 import { render as rtlRender } from '@testing-library/react'
-import {applyMiddleware, compose, createStore} from 'redux';
-import createSagaMiddleware from "redux-saga";
 import { Provider } from 'react-redux'
 import { ToastProvider } from "react-toast-notifications";
 import rootReducer from "../modules";
 import ReduxThunk from "redux-thunk";
-import { createBrowserHistory } from "history";
+import configureStore from 'redux-mock-store';
 import { Router } from "react-router-dom";
 
 // TODO: if react-scripts support jest 25, delete jest-environment-jsdom-sixteen
-const customHistory = createBrowserHistory();
-const sagaMiddleware = createSagaMiddleware({
-  context: {
-    history: customHistory
-  }
-});
-let middleware = [ReduxThunk.withExtraArgument({ history: customHistory }), sagaMiddleware];
-let middlewareWrapper = compose(applyMiddleware(...middleware));
+function makeStore (customHistory, customState = {}) {
+  let middleware = [];
+  middleware = [...middleware, ReduxThunk.withExtraArgument({ history: customHistory })];
+  const mockStore = configureStore(middleware)
+  const store = mockStore(customState);
+  store.replaceReducer(rootReducer)
+
+  return store;
+}
+
+
 function render(
   ui,
   {
-    store = createStore(rootReducer, middlewareWrapper),
+    customHistory,
+    store,
     ...renderOptions
   } = {}
 ) {
@@ -43,4 +45,4 @@ function render(
 export * from '@testing-library/react'
 
 // override render method
-export { render }
+export { render, makeStore }
