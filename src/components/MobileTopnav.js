2aaea6c82a644154bc4e839d13b77983022d669d
Yunsup Sim <pedogunu@gmail.com>
2020-08-21 01:40:03 +0900

    feat: Change mobile nav design

--- src/components/MobileTopnav.js
+++ src/components/MobileTopnav.js
@@ -4,31 +4,28 @@ import { Link } from 'react-router-dom';
 
 const Container = styled.div`
   position: fixed;
-  top: 55px;
+  top: 0;
   left: 0;
   width: 100%;
-  height: calc(100% - 153px);
+  height: 100%;
   min-width: 360px;
-  background: #f7f7f7;
+  background: white;
   padding-bottom: 40px;
   
   border-bottom: rgba(210, 218, 226, 0.5) 1px solid;
   transition: all .3s cubic-bezier(0, 0, 0.2, 1);
-  transform: ${props => props.mobileMenu ? 'none' : 'translateX(-100%)'};
+  transform: ${props => props.mobileMenu ? 'none' : 'translateX(100%)'};
   z-index: 20;
   overflow: scroll;
 `;
 
 const UserInfoSection = styled.div`
   width: 100%;
-  height: 41px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+  height: 119px;
   color: #175c8e;
   font-size: 14px;
   font-family: NanumSquare;
-  padding: 0 20px;
+  padding: 0 16px;
   box-sizing: border-box;
   background: #ffffff;
   position: sticky;
@@ -37,13 +34,47 @@ const UserInfoSection = styled.div`
   left: 0;
 `;
 
+const BackRouteButton = styled.button`
+  display: block;
+  margin: 5px 0 8px -2px;
+  width: 28px;
+  height: 28px;
+  padding: 0;
+  border: 0;
+  background: url("http://static.koreatech.in/assets/img/arrow_left.png") center/28px 28px no-repeat;
+`;
+
 const Nickname = styled.span`
   font-size: 16px;
-  display: inline-block;
-  width: 190px;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
+  display: block;
+  width: 240px;
+  margin-bottom: 16px;
+  font-weight: 500;
+  
+  ::after {
+    content: '님, 안녕하세요!';
+    font-size: 13px;
+  font-weight: normal;
+  }
+`;
+
+const MyInfoLink = styled(Link)`
+  text-decoration: none;
+  font-size: 15px;
+  width: 80px;
+  font-weight: normal;
+  color: black;
+  display: flex;
+  line-height: 24px;
+  
+  :before {
+    display: inline-block;
+    width: 24px;
+    height: 24px;
+    margin-right: 4px;
+    content: '';
+    background: url("http://static.koreatech.in/assets/img/ic-bottom_myinfo.png") center/24px 24px;
+  }
 `;
 
 const AuthLinkStyle = css`
@@ -69,9 +100,12 @@ const AuthLinkButton = styled.span`
 const TitleSection = styled.div`
   width: 100%;
   box-sizing: border-box;
-  padding: 24px 20px 10px 20px;
-  font-size: 14px;
+  padding: 5px 16px 5px;
+  margin-bottom: 8px;
+  font-size: 15px;
+  font-weight: normal;
   color: #858585;
+  background-color: #f7f7f7;
   text-align: left;
 `;
 
@@ -83,9 +117,8 @@ const SubMenuWrapper = styled.div`
 const SubMenuLinkButton = styled(Link)`
   display: block;
   width: 50%;
+  font-size: 15px;
   background: #fff;
-  border: 0.5px rgba(210, 218, 226, 0.5) solid;
-  font-size: 16px;
   box-sizing: border-box;
   border-collapse: collapse;
   cursor: pointer;
@@ -100,6 +133,28 @@ const SubMenuLinkButton = styled(Link)`
     font-weight: 700;
     outline: none;
   }
+`;
+
+const BCSDLabLogo = styled.img.attrs({
+  src: 'http://static.koreatech.in/assets/img/ic-bcsd_gray.png',
+  alt: 'BCSD Lab 로고'
+})`
+  position: absolute;
+  right: 16px;
+  bottom: 53px;
+  width: 33px;
+  height: 33px;
+`;
+
+const KOINLogo = styled.img.attrs({
+  src: 'http://static.koreatech.in/assets/img/rectangle_icon.png',
+  alt: 'KOIN 컬러 로고'
+})`
+  position: absolute;
+  right: 16px;
+  top: 16px;
+  width: 40px;
+  height: 40px;
 `;
 
 
@@ -121,13 +176,14 @@ export default function MobileTopnav({
   return (
     <Container mobileMenu={mobileMenu}>
       <UserInfoSection>
+        <BackRouteButton onClick={() => setMobileMenu(false)} />
         <Nickname>
           {userInfo && userInfo.nickname}
         </Nickname>
         <div>
           {token && 
             <>
-              <StyledAuthLink to="/modifyinfo" onClick={onCloseNav}>정보수정</StyledAuthLink>|
+              <MyInfoLink to="/modifyinfo" onClick={onCloseNav}>내 정보</MyInfoLink>|
               <AuthLinkButton onClick={() => {onLogout(); onCloseNav()}}>로그아웃</AuthLinkButton>
             </>
           }
@@ -151,9 +207,10 @@ export default function MobileTopnav({
             </SubMenuLinkButton>
           ))}
           </SubMenuWrapper>
-          
         </div>
       ))}
+      <KOINLogo />
+      <BCSDLabLogo />
     </Container>
   )
 }
